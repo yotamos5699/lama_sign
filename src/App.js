26a301d5be5d6a0b039c428e91c920d29b4d4c52
yotamos5699 <m.lhafakot@gmail.com>
2022-07-28 16:25:33 +0300

    -m maleeed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,107 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import PdfView from "./components/PdfView";
 import { Worker } from "@react-pdf-viewer/core";
 import Sign from "./components/Sign";
-import demo from "./assets/bb.pdf";
-import demo2 from "./assets/dd.pdf";
-import demo3 from "./assets/demo.pdf";
+import aa from "./assets/aa.PDF";
+import bb from "./assets/bb.pdf";
+import cc from "./assets/cc.PDF";
+import dd from "./assets/dd.pdf";
+import ee from "./assets/ee.pdf";
+import ff from "./assets/ff.PDF";
+import gg from "./assets/gg.pdf";
+import hh from "./assets/hh.PDF";
+import ii from "./assets/ii.PDF";
+import jj from "./assets/jj.pdf";
 import Modal from "react-modal";
 
 import "./styles.css";
-
+import Auth from "./components/Auth";
+//import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 Modal.setAppElement("#root");
 let pdfs = [
   {
     isSIgned: false,
-    DocNumber: 3230,
+    DocNumber: 3276,
     AccountKey: "6107",
-    DocumentDetails: "0506655699",
-    DocUrl: demo,
+    DocumentDetails: "0505455699",
+    DocUrl: aa,
+  },
+  {
+    isSIgned: false,
+    DocNumber: 4530,
+    AccountKey: "6900",
+    DocumentDetails: "05066558764",
+    DocUrl: bb,
+  },
+  {
+    isSIgned: false,
+    DocNumber: 2312,
+    AccountKey: "6207",
+    DocumentDetails: "0506612313",
+    DocUrl: cc,
+  },
+  {
+    isSIgned: false,
+    DocNumber: 3320,
+    AccountKey: "6707",
+    DocumentDetails: "0506655632",
+    DocUrl: dd,
+  },
+  {
+    isSIgned: false,
+    DocNumber: 3530,
+    AccountKey: "6531",
+    Phone: "0501231231",
+    DocUrl: ee,
   },
   {
     isSIgned: false,
     DocNumber: 3230,
-    AccountKey: "6107",
-    DocumentDetails: "0506655699",
-    DocUrl: demo2,
+    AccountKey: "6051",
+    Phone: "0506655698",
+    DocUrl: ff,
+  },
+  {
+    isSIgned: false,
+    DocNumber: 4230,
+    AccountKey: "6031",
+    Phone: "0509980680",
+    DocUrl: gg,
   },
   {
     isSIgned: false,
     DocNumber: 3230,
-    AccountKey: "6107",
-    DocumentDetails: "0506655699",
-    DocUrl: demo3,
+    AccountKey: "6031",
+    Phone: "0509980680",
+    DocUrl: hh,
   },
+  {
+    isSIgned: false,
+    DocNumber: 3254,
+    AccountKey: "6107",
+
+    Phone: "0506655699",
+    DocUrl: ii,
+  },
+
+  {
+    isSIgned: false,
+    DocNumber: 3342,
+    AccountKey: "6107",
+    Phone: "0506655699",
+    DocUrl: jj,
+  },
+];
+
+let castumersList = [
+  { value: "6107", label: "רמי" },
+  { value: "6900", label: "איציק" },
+  { value: "6031", label: "משה" },
+  { value: "6051", label: "טל" },
+  { value: "6531", label: "רמי" },
+  { value: "6707", label: "דותן לוי" },
+  { value: "6051", label: "סמי הכבאי" },
+  { value: "6207", label: "ילד הומו" },
 ];
 
 export default function App() {
@@ -40,18 +110,46 @@ export default function App() {
   const [files, setFiles] = useState();
   const [isMounted, setIsMounted] = useState(false);
   const [courentFile, setCurrentFile] = useState(null);
+  const [sortedCastumers, setSortedCastumers] = useState();
+  const [isSelected, setIsSelected] = useState(false);
+  const [docsObject, setDocsObject] = useState();
+
+  const urlsToRender = useRef();
+  const dropValRef = useRef();
+
   useEffect(() => {
     const setData = async () => {
-      Promise.all(pdfs.map((doc) => doc.DocUrl))
-        .then((res) => {
-          setFiles(res);
-          console.log(res);
-        })
-        .catch((err) => console.group(err));
+      setSortedCastumers(castumersList);
+      setDocsObject(pdfs);
     };
     setData();
   }, []);
 
+  const loadPdfs = async (urls) => {
+    Promise.all(urls.map((doc) => doc.DocUrl))
+      .then((res) => {
+        setFiles(res);
+        console.log(res);
+      })
+      .catch((err) => console.group(err));
+  };
+  const handleDropDownInput = (event) => {
+    dropValRef.current = event;
+    console.log("ebent", event);
+    console.log("dropValue  ", dropValRef);
+
+    let asignUrls = [];
+
+    docsObject.forEach((doc, index) => {
+      dropValRef.current.forEach((value) => {
+        if (doc.AccountKey == value.value) {
+          asignUrls.push(files[index]);
+        }
+      });
+      console.log(asignUrls);
+      urlsToRender.current = asignUrls;
+    });
+  };
   function toggleModal(e) {
     e.preventDefault();
     setIsOpen(!isOpen);
@@ -63,6 +161,12 @@ export default function App() {
     console.log(isMounted);
     console.log(files);
   };
+  const handleAuthButton = (event) => {
+    loadPdfs(urlsToRender)
+      .then((res) => setFiles(res))
+      .then(setIsSelected(true))
+      .catch((err) => console.log(err));
+  };
 
   const passPdfToSign = (e, fi) => {
     console.log("ahhahaahahahah");
@@ -72,9 +176,14 @@ export default function App() {
 
   return (
     <div className="app">
+      <Auth
+        handleDropDownInput={handleDropDownInput}
+        handleAuthButton={handleAuthButton}
+        sortedCastumers={sortedCastumers}
+      />
       <button onClick={setDataToFile}>!!!!!!!! למה </button>
       <Worker workerUrl={packageURL}>
-        {isMounted ? (
+        {isSelected && isMounted ? (
           files.map((fi) => {
             return (
               <div>
@@ -106,7 +215,7 @@ export default function App() {
         closeTimeoutMS={500}
       >
         <Sign
-          demo={isMounted && courentFile && courentFile}
+          demo={isSelected && isMounted && courentFile && courentFile}
           setResult={setResult}
         />
       </Modal>
